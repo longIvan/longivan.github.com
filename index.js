2a46e6bc2c328e8e789a8cea7ae9f35653485777
langyue <langyue@pinduoduo.com>
2019-05-27 20:44:00 +0800

    feat: clear repository

--- index.js
+++
@@ -1,6 +0,0 @@
-const Browser = require('./Browser.js');
-const info = new Browser(
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/18.17763'
-);
-
-console.log(info);
